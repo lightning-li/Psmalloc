1999888bb1643a03c3a5e3ff1de486b8cf5c5b8e
FillZpp <FillZpp.pub@gmail.com>
2014-07-17 16:02:21 +0000

    change

--- src/psmalloc.h
+++ src/psmalloc.h
@@ -7,7 +7,6 @@
 #define PSMALLOC_PSMALLOC_H_
 
 
-#include <malloc.h>           // for struct mallinfo
 #include <sys/cdefs.h>        // for __THROW
 #include <stddef.h>           // for size_t
 #include "core_config.h"
@@ -25,6 +24,6 @@ void ps_cfree (void *ptr) __THROW;
 void *ps_realloc (void *ptr, size_t size) __THROW;
 void *ps_memalign (size_t align, size_t size) __THROW;
 void *ps_valloc (size_t size) __THROW;
-//struct mallinfo ps_mallinfo(void);
+
 
 #endif
